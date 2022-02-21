0182b8058b275d994325a6602c38868999a4f377
ngas-wakefield <ngaswakefield@gmail.com>
2022-02-21 13:36:51 +1300

    added scroll to top button

--- script.js
+++ script.js
@@ -23,3 +23,27 @@ window.addEventListener('resize', () => {
 function toggleOpen () {
   document.getElementById("mySidebar").classList.toggle("open");
 }
+
+
+
+/* top scroll button */
+
+//Get the button
+var mybutton = document.getElementById("myTopBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
